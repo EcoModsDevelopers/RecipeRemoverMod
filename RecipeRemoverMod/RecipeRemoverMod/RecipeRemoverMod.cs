1ccffbcfa87acce579ba0928fe32512c8fbc4dde
lordmampf <lordmampf@users.noreply.github.com>
2018-12-04 23:16:56 +0100

    first commit

---
+++ RecipeRemoverMod/RecipeRemoverMod/RecipeRemoverMod.cs
@@ -0,0 +1,57 @@
+﻿using Asphalt;
+using Asphalt.Service;
+using Asphalt.Storeable;
+using Asphalt.Utils;
+using Eco.Core.Plugins.Interfaces;
+using Eco.Gameplay.Components;
+using Eco.Gameplay.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeRemoverMod
+{
+    [AsphaltPlugin("RecipeRemoverMods")]
+    public class RecipeRemoverMod : IModKitPlugin
+    {
+        [Inject]
+        [StorageLocation("Config")]
+        public static IStorage ConfigStorage { get; set; }
+
+        public void OnPostEnable()
+        {
+            try
+            {
+                Dictionary<Type, Recipe[]> staticRecipes = (Dictionary<Type, Recipe[]>)typeof(CraftingComponent).GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(x => x.Name.Contains("staticRecipes")).GetValue(null);
+                var keyTypes = staticRecipes.Values.SelectMany(r => r).Select(r => r.GetType());
+
+                foreach (var type in keyTypes.ToArray()) //copy list because it will be modified from RemoveRecipe
+                {
+                    bool? config = ConfigStorage.Get<bool?>(type.FullName);
+
+                    if (config == null)
+                    {
+                        ConfigStorage.Set<bool>(type.FullName, false);
+                        continue;
+                    }
+
+                    if (config == true)
+                        RecipeRemover.RemoveRecipe(type);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public string GetStatus()
+        {
+            return "running...";
+        }
+    }
+}
